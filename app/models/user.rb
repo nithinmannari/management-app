54c3bc409f8b49591c7c26333081762d868ab023
Nithin Manari Venkata Satya <nithin.mannari@gmail.com>
2016-10-26 16:27:17 +0000

    Add devise and milia for multitenancy

---
+++ app/models/user.rb
@@ -0,0 +1,26 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :confirmable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  acts_as_universal_and_determines_account
+  has_one :member, :dependent => :destroy
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :confirmable, :confirmable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  acts_as_universal_and_determines_account
+  has_one :member, :dependent => :destroy
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :confirmable, :confirmable, :confirmable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  acts_as_universal_and_determines_account
+  has_one :member, :dependent => :destroy
+
+end
