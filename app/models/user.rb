a1152d74cac97a3a0d41c0868f575db03a983128
Nithin Manari Venkata Satya <nithin.mannari@gmail.com>
2016-11-03 19:11:52 +0000

    Add Users View to the project

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
+         
 
   acts_as_universal_and_determines_account
   has_one :member, :dependent => :destroy
@@ -22,9 +23,13 @@ class User < ActiveRecord::Base
 
   acts_as_universal_and_determines_account
   has_one :member, :dependent => :destroy
+  has_many :user_projects
+  has_many :projects, through: :user_projects
   
   def is_admin?
   is_admin
   end
+  
+  
 
 end
